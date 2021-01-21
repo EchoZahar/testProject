f3b44553fe50d35f705a6209443ec8fd57f436ce
ZaharYamal <zahar.yamal@yandex.ru>
2021-01-22 00:11:53 +0400

    Реализация ролей и разрешении пользователей. Созданы 2 таблицы roles и permissions + 3 сводных таблицы. Пользователь может иметь много Прав и много Ролей. То же самое наоборот, Роль может иметь много Пользователей, а Право может иметь много Пользователей. Добавлен bootstrap. Auth. Добавлены тестовые роли, права и пользователи в файлах: PermissionSeeder, RoleSeeder, UserSeeder.

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $manager = new Role();
+        $manager->name = 'Project Manager';
+        $manager->slug = 'project-manager';
+        $manager->save();
+        $developer = new Role();
+        $developer->name = 'Web Developer';
+        $developer->slug = 'web-developer';
+        $developer->save();
+        $administrator = new Role();
+        $administrator->name = 'administrator';
+        $administrator->slug = 'administrator';
+        $administrator->save();
+    }
+}
